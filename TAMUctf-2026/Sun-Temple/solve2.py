1e37692f5b8fdbff4fb1c74bf5478d11a1948657
CongMinh8365 <doancongminh69696969@gmail.com>
2026-03-25 07:31:46 -0400

    Update solve2.py

--- TAMUctf-2026/Sun-Temple/solve2.py
+++ TAMUctf-2026/Sun-Temple/solve2.py
@@ -6,7 +6,7 @@ r = remote("streams.tamuctf.com", 443, ssl=True, sni="sun-temple")
 # 2. Doi server in ra dong chu chao mung
 r.recvuntil(b"we await your offering> ")
 
-# 3. Gui payload tuyet hao cua chung ta len
+# 3. Gui payload cua chung ta len
 payload = b"10f1a20384653657e8b92acb"
 r.sendline(payload)
 
@@ -17,6 +17,3 @@ print("\n" + "="*50)
 print("[+] Da nhan duoc phan hoi tu server:")
 print(result)
 print("="*50 + "\n")
-
-# Chot ha
-# r.interactive() # Da lay duoc flag roi thi khong can interactive nua
